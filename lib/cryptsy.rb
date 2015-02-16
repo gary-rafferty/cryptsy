9547281e5c47e3316a51926d6d32080c2498fe1c
Gary Rafferty <gary.rafferty@gmail.com>
2015-02-16 23:06:28 +0000

    Add basic structure for client module

--- lib/cryptsy.rb
+++ lib/cryptsy.rb
@@ -1,5 +1,33 @@
 require "cryptsy/version"
+require "httparty"
 
 module Cryptsy
-  # TODO
+
+  @api_base = 'https://api.cryptsy.com/api/v2'
+
+  @api_key = nil
+
+  def self.api_key=(api_key)
+    @api_key = api_key
+  end
+
+  def self.api_key
+    @api_key
+  end
+
+  def self.api_base
+    @api_base
+  end
+
+  def self.get(path)
+  end
+
+  def self.post(path, data)
+  end
+
+  def self.put(path, data)
+  end
+
+  def delete(path)
+  end
 end
--- spec/cryptsy_spec.rb
+++ spec/cryptsy_spec.rb
@@ -3,8 +3,19 @@ require './spec/spec_helper.rb'
 class CryptsySpec < Minitest::Spec
 
   describe 'cryptsy' do
-    it 'should pass' do
-      pass
+    it 'should have an api_base' do
+      Cryptsy.api_base.wont_be_nil
+      Cryptsy.api_base.must_equal 'https://api.cryptsy.com/api/v2'
+    end
+
+    it 'should not set an api_key' do
+      Cryptsy.api_key.must_be_nil
+    end
+
+    it 'should provide a setter for api_key' do
+      Cryptsy.api_key= 'api_key'
+      Cryptsy.api_key.wont_be_nil
+      Cryptsy.api_key.must_equal 'api_key'
     end
   end
 end
