3fe9549f0d65e5359971edbbe19af840ee565600
Gary Rafferty <gary.rafferty@gmail.com>
2015-02-17 09:35:47 +0000

    Remove unneeded test

--- spec/cryptsy_spec.rb
+++ spec/cryptsy_spec.rb
@@ -8,10 +8,6 @@ class CryptsySpec < Minitest::Spec
       Cryptsy.api_base.must_equal 'https://api.cryptsy.com/api/v2'
     end
 
-    it 'should not set an api_key' do
-      Cryptsy.api_key.must_be_nil
-    end
-
     it 'should provide a setter for api_key' do
       Cryptsy.api_key= 'api_key'
       Cryptsy.api_key.wont_be_nil
