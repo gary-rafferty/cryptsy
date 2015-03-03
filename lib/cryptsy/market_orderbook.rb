5fd493084ce8fb64b3bcddf4d6edbd67ddd07a8d
Gary Rafferty <gary.rafferty@gmail.com>
2015-03-03 23:04:25 +0000

    Use named arguments for defaults and add spec for overriding

--- lib/cryptsy/market_orderbook.rb
+++ lib/cryptsy/market_orderbook.rb
@@ -1,11 +1,17 @@
 module Cryptsy
   class MarketOrderbook < ApiResource
+
     def initialize(attrs)
       super
     end
 
-    def self.find(market_id)
-      response = Cryptsy.get("/markets/#{market_id}/orderbook")
+    def self.find(market_id, limit: 100, type: 'Both', mine: false)
+      path = "/markets/#{market_id}/orderbook"
+      
+      query = URI.encode_www_form({limit: limit, type: type, mine: mine})
+      path = "#{path}?#{query}"
+
+      response = Cryptsy.get(path)
 
       self.new(response)
     end
--- spec/market_orderbook_spec.rb
+++ spec/market_orderbook_spec.rb
@@ -4,7 +4,7 @@ class MarketOrderbookSpec < Minitest::Spec
 
   describe 'market_orderbook class' do
     it 'exposes a .find() method and returns a single MarketOrderbook instance' do
-      Cryptsy.stubs(:get).returns(
+      Cryptsy.stubs(:get).with("/markets/1/orderbook?limit=100&type=Both&mine=false").returns(
         {
           "buy_orders" => [
             {"price"=>241.00000227, "quantity"=>0.0002, "total"=>0.0482},
@@ -21,6 +21,26 @@ class MarketOrderbookSpec < Minitest::Spec
 
       volume.must_be_instance_of Cryptsy::MarketOrderbook
     end
+
+    it 'exposes a .find() method with overridable defaults and returns a single MarketOrderbook instance' do
+      Cryptsy.stubs(:get).with("/markets/2/orderbook?limit=2&type=Buy&mine=true").returns(
+        {
+          "buy_orders" => [
+            {"price"=>241.00000227, "quantity"=>0.0002, "total"=>0.0482},
+            {"price"=>241.00000227, "quantity"=>0.0002, "total"=>0.0482} 
+          ]
+        }
+      )
+
+      volume = Cryptsy::MarketOrderbook.find('2', limit: 2, type: 'Buy', mine: true)
+
+      volume.must_be_instance_of Cryptsy::MarketOrderbook
+
+      assert_raises NoMethodError do
+        volume.sellorders
+      end
+    end
+
   end
 end
  
